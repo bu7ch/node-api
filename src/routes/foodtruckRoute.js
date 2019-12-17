6234b9d6e9d4177283e646ca82d6a89193661926
Rodolphe <dutchr@live.fr>
2019-12-17 17:01:10 +0100

    all routes works

--- src/routes/foodtruckRoute.js
+++ src/routes/foodtruckRoute.js
@@ -17,28 +17,29 @@ ftRouter.post('/add', (req, res) => {
   const newFoodtruck = new Foodtruck(req.body)
   newFoodtruck.save((err, foodtruck) => {
     if (err) console.error(err)
-    res.json(foodtruck)
+    res.redirect('/api/v1/foodtrucks/all')
   })
 })
-ftRouter.route('/:id')
+ftRouter.route('/edit/:id')
   .get((req,res) => {
     Foodtruck.findById(req.params.id, (err, foodtruck) => {
       if (err) console.error(err)
-      res.json(foodtruck)
+      res.render('edit' ,{foodtruck:foodtruck})
     })
   })
-  .put((req, res)=>{
+  .post((req, res)=>{
     Foodtruck.findById({_id: req.params.id}, (err, foodtruck)=> {
       if (err) console.error(err)
       Object.assign(foodtruck, req.body).save((err, foodtruck) => {
         if (err) console.error(err)
-        res.json({ message:"foodtruck MaJ", foodtruck})
+        res.redirect('/api/v1/foodtrucks/all')
       })
     })
   })
-  .delete((req, res)=> {
+  ftRouter.get('/delete/:id',(req, res)=> {
     Foodtruck.remove({_id: req.params.id}, (err, result) => {
-      res.json({ message: 'foodtruck successfully deleted!!', result})
+      // res.json({ message: 'foodtruck successfully deleted!!', result})
+      res.redirect('/api/v1/foodtrucks/all')
     })
   })
 
